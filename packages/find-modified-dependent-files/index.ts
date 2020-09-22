221e00ce171b810320888c44e50bd5ac14aa95b3
Nick Heiner <nheiner@netflix.com>
2020-09-21 17:22:16 -0700

    Add initial implementation, but TS does not build

--- packages/find-modified-dependent-files/index.ts
+++ packages/find-modified-dependent-files/index.ts
@@ -1,1 +1,47 @@
 #! /usr/bin/env node
+
+import yargs from 'yargs';
+import dependencyTree from 'dependency-tree';
+import 'loud-rejection/register';
+import execa from 'execa';
+import _ from 'lodash';
+
+const {argv} = yargs
+  .options({
+    compareRef: {
+      type: 'string',
+      default: 'HEAD',
+      description: 'The changed ref to compare with.'
+    },
+    mainRef: {
+      type: 'string',
+      default: 'origin/master',
+      description: 'The upstream branch to compare with.'
+    },
+    file: {
+      required: true,
+      type: 'string',
+      description: 'The file whose dependents you are investigating' 
+    },
+    allFiles: {
+      required: true,
+      type: 'string',
+      description: 'The directory that contains all the files you could possibly be looking for' 
+    }
+  })
+  .help();
+
+async function main() {
+  const dependentFiles = dependencyTree.toList({
+    filename: argv.file,
+    directory: argv.allFiles
+  });
+
+  const mergeBase = await execa('git', ['merge-base', argv.compareRef, argv.mainRef]);
+  const changedFiles = await execa('git', ['diff', `${mergeBase}..${argv.compareRef}`]);
+
+  console.log(_.intersection(dependentFiles, changedFiles));
+}
+
+main();
+
